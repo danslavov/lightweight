d84cf622c5dae9cc6ad9393eabd19ad243c78b93
danslavov <danslavov@gmail.com>
2021-12-28 20:32:56 +0200

    sync

--- scripts/train.py
+++ scripts/train.py
@@ -1,3 +1,4 @@
+import gc
 import os
 import sys
 import time
@@ -16,7 +17,6 @@ import torch
 import torch.nn as nn
 import torch.utils.data as data
 import torch.backends.cudnn as cudnn
-from torchvision.utils import save_image
 from torchvision import transforms
 from light.utils.distributed import *
 from light.utils.logger import setup_logger
@@ -39,7 +39,7 @@ def parse_args():
                         help='base image size')
     parser.add_argument('--crop-size', type=int, default=700,
                         help='crop image size')
-    parser.add_argument('--workers', '-j', type=int, default=1,
+    parser.add_argument('--workers', '-j', type=int, default=2,          # ------------------WORKERS------------
                         metavar='N', help='dataloader threads')
     # training hyper params
     parser.add_argument('--ohem', action='store_true', default=False,
@@ -50,11 +50,11 @@ def parse_args():
                         help='auxiliary loss weight')
     parser.add_argument('--batch-size', type=int, default=32, metavar='N',          # ---------BATCH------------
                         help='input batch size for training (default: 4)')
-    parser.add_argument('--start_epoch', type=int, default=0,
+    parser.add_argument('--start-epoch', type=int, default=71,
                         metavar='N', help='start epochs (default:0)')
     parser.add_argument('--epochs', type=int, default=999, metavar='N',
                         help='number of epochs to train (default: 240)')
-    parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
+    parser.add_argument('--lr', type=float, default=0.00005, metavar='LR',
                         help='learning rate (default: 1e-4)')
     parser.add_argument('--momentum', type=float, default=0.9, metavar='M',
                         help='momentum (default: 0.9)')
@@ -123,7 +123,8 @@ class Trainer(object):
         if not args.skip_val:
             valset = get_segmentation_dataset(args.dataset, split='val', mode='val', **data_kwargs)
             val_sampler = make_data_sampler(valset, False, args.distributed)
-            val_batch_sampler = make_batch_data_sampler(val_sampler, args.batch_size)
+            # val_batch_sampler = make_batch_data_sampler(val_sampler, args.batch_size)  # INFO: orig
+            val_batch_sampler = make_batch_data_sampler(val_sampler, 4)  # INFO: small batch size only for validation, to avoid cuda overflow TODO: use orig, if needed
             self.val_loader = data.DataLoader(dataset=valset,
                                               batch_sampler=val_batch_sampler,
                                               num_workers=args.workers,
@@ -202,68 +203,58 @@ class Trainer(object):
         epochs_per_val = self.args.val_epoch
         # save_per_iters = self.args.save_epoch * self.args.iters_per_epoch
         iter_per_epoch = self.args.iters_per_epoch
-        epoch = 1
+        epoch = self.args.start_epoch
 
         # logger.info('Start training, Total Epochs: {:d} = Total Iterations {:d}'.format(epochs, max_iters))
+        # logger.info('batch size: {} | workers: {}\n'.format(self.args.batch_size, self.args.workers))
 
         self.model.train()
         freeze_encoder(self.model)
+
         start = time.time()
+        # duration_all_iterations = 0
+        # print(iter_per_epoch)
 
         for iteration, (images, targets) in enumerate(self.train_loader):
-
-            # img_path = self.train_loader.dataset.images[iteration]
-            # mask_path = self.train_loader.dataset.mask_paths[iteration]
-
             iteration += 1  # TODO: why iteration is not incremented automatically?
 
-            start_iter_time = time.time()
+            # start_iter_time = time.time()
 
             images = images.to(self.device)
             targets = targets.to(self.device)
-
-            outputs = self.model(images)
-
-
-            # visual_check(img_path, mask_path, outputs[0][0].cpu())
-
-
+            outputs = self.model(images)  # INFO: outputs have 4 channels (1 for alpha)
             loss_dict = self.criterion(outputs, targets)
             losses = sum(loss for loss in loss_dict.values())
-
-            # reduce losses over all GPUs for logging purposes
-            # loss_dict_reduced = reduce_loss_dict(loss_dict)
-            # losses_reduced = sum(loss for loss in loss_dict_reduced.values())
-
             self.optimizer.zero_grad()
             losses.backward()
             self.optimizer.step()
-
             self.metric.update(outputs[0], targets)
 
-            iter_duration = time.time() - start_iter_time
-            iter_pixAcc, iter_mIoU = self.metric.get()
-            logger.info('iteration {} || iter_time: {} | iter_pixAcc: {} | iter_mIoU: {} | iter_combined: {}'
-                        .format(iteration, iter_duration, iter_pixAcc, iter_mIoU, iter_pixAcc + iter_mIoU))
+            # log per-iteration stats
+            # iter_duration = time.time() - start_iter_time
+            # iter_pixAcc, iter_mIoU = self.metric.get()
+            # logger.info('iteration {} || iter_time: {} | iter_pixAcc: {} | iter_mIoU: {} | iter_combined: {}'
+            #             .format(iteration, iter_duration, iter_pixAcc, iter_mIoU, iter_pixAcc + iter_mIoU))
+            #
+            # duration_all_iterations += iter_duration
 
             # at the end of the current epoch:
             if iteration % iter_per_epoch == 0:
                 epoch_duration = time.time() - start
                 training_loss = float(losses)
                 pixAcc, mIoU = self.metric.get()
-                logger.info('Epoch: {0:03d} | Time: {4:.4f} | Avg. loss: {1:.4f} | pixAcc: {2:.4f} | Mean IoU: {3:.4f} | combined: {5:.4f}'
-                            .format(epoch, training_loss, pixAcc, mIoU, epoch_duration, pixAcc + mIoU))
-
-                # TODO visual_check()
+                logger.info('Epoch: {0:03d} | Time: {1:.4f} | Avg. loss: {2:.4f} | pixAcc: {3:.4f} | Mean IoU: {4:.4f} | combined: {5:.4f}'
+                            .format(epoch, epoch_duration, training_loss, pixAcc, mIoU, pixAcc + mIoU))
 
                 # validate each n epochs (n = epochs_per_val):
                 if epoch % epochs_per_val == 0:
                     self.validation(epoch)
                     self.model.train()
                     freeze_encoder(self.model)
+
                 epoch += 1  # increment epoch counter
                 start = time.time()  # reset time
-                torch.cuda.empty_cache()
+                # torch.cuda.empty_cache()
 
             self.lr_scheduler.step()
 
@@ -274,9 +265,12 @@ class Trainer(object):
         #     "Total training time: {} ({:.4f}s / it)".format(
         #         total_training_str, total_training_time / max_iters))
 
+            img_load_start = time.time()
+
     def validation(self, epoch):
         # total_inter, total_union, total_correct, total_label = 0, 0, 0, 0
         # is_best = False
+        gc.collect()
         torch.cuda.empty_cache()
         self.metric.reset()
         if self.args.distributed:
@@ -286,7 +280,6 @@ class Trainer(object):
 
         val_loss = 0.0
 
-        torch.cuda.empty_cache()
         model.eval()
         start = time.time()
 
@@ -306,8 +299,8 @@ class Trainer(object):
         avg_val_loss = val_loss / len(self.val_loader)
         pixAcc, mIoU = self.metric.get()
 
-        logger.info('VALIDATION | Time: {3:.4f} | Avg. loss: {0:.4f} | pixAcc: {1:.4f} | Mean IoU: {2:.4f} | combined: {4:.4f}'
-                    .format(avg_val_loss, pixAcc, mIoU, duration, pixAcc + mIoU))
+        logger.info('VALIDATION | Time: {0:.4f} | Avg. loss: {1:.4f} | pixAcc: {2:.4f} | Mean IoU: {3:.4f} | combined: {4:.4f}'
+                    .format(duration, avg_val_loss, pixAcc, mIoU, pixAcc + mIoU))
 
         new_pred = (pixAcc + mIoU) / 2  # TODO: describe that pixel accuracy is also used
         if new_pred > self.best_pred:
@@ -370,5 +363,18 @@ if __name__ == '__main__':
     logger.info('\n')
 
     trainer = Trainer(args)
+
+    # Examine optimal combination of workers and batch size
+    # for batch_size in (2, 4, 8, 16, 32):
+    #     args.batch_size = batch_size
+    #     for num_workers in range(5):
+    #         args.workers = num_workers
+    #         try:
+    #             trainer.train()
+    #         except:
+    #             print('ERROR')
+    #             continue
+    # exit()
+
     trainer.train()
     torch.cuda.empty_cache()
